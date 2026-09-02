b2cae66510bbbd5b4ce39694ce1d98fded318ef8
Jose Echavez M <jose.echavez92@gmail.com>
2026-09-02 18:13:01 -0500

    update sh script

--- run-global-sura.sh
+++ run-global-sura.sh
@@ -11,6 +11,10 @@ export PATH="/root/.nvm/versions/node/v24.20.0/bin:$PATH"
 echo "Running global sura script"
 echo "Current directory: $(pwd)"
 
+TIMES_RUN=$((RANDOM % 3 + 3)) # Between 3 and 5 times
+echo "Times to run: $TIMES_RUN"
+SLEEP_TIME=$((RANDOM % 21 + 20)) # Between 20 and 40 seconds
+echo "Sleep time between runs: $SLEEP_TIME seconds"
 
 # Now you can use standard command names safely
 NPM_VERSION=$(npm --version)
@@ -18,7 +22,13 @@ echo "NPM version: $NPM_VERSION"
 
 
 # Run the global sura script
-npm run global-sura 3 10
+for i in $(seq 1 $TIMES_RUN); do
+  sleep $SLEEP_TIME
+  echo "Running global sura iteration $i"
+  DELAY_TIME=$((RANDOM % 3 + 1)) # Between 1 and 3 seconds
+  npm run global-sura $DELAY_TIME 120
+done
 
 current_time=$(date "+%Y-%m-%d %H:%M:%S")
 echo "Global sura script completed at $current_time"
+

534cfdd4bd04e67986a14cbae74a60798cd1df93
Jose Echavez M <jose.echavez92@gmail.com>
2026-09-02 17:43:14 -0500

    update sh script

--- run-global-sura.sh
+++ run-global-sura.sh
@@ -3,17 +3,20 @@
 
 cd $(dirname $0)
 
+# Explicitly add the NVM node binary folder to the PATH
+export PATH="/root/.nvm/versions/node/v24.20.0/bin:$PATH"
+
 echo "Running global sura script"
 echo "Current directory: $(pwd)"
 
 
-# get npm path
-NPM_VERSION=$(/root/.nvm/versions/node/v24.20.0/bin/npm --version)
+# Now you can use standard command names safely
+NPM_VERSION=$(npm --version)
 echo "NPM version: $NPM_VERSION"
 
 
-# Run the global sura script and get the output
-/root/.nvm/versions/node/v24.20.0/bin/npm run global-sura 3 10
+# Run the global sura script
+npm run global-sura 3 10
 
 current_time=$(date "+%Y-%m-%d %H:%M:%S")
 echo "Global sura script completed at $current_time"

14636cc25b1950102e98b05a02053077812928fa
Jose Echavez M <jose.echavez92@gmail.com>
2026-09-02 17:34:47 -0500

    update sh script

--- run-global-sura.sh
+++ run-global-sura.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 #
-# crontab -e run at 2am
-# 0 2 * * * bash $HOME/echamor-app/scripts/daily_backup.sh >> $HOME/upload.log
-#
-#
+
 cd $(dirname $0)
 
 echo "Running global sura script"
 echo "Current directory: $(pwd)"
 
 # get npm path
-NPM_PATH=$(which npm)
-echo "NPM path: $NPM_PATH"
+NPM_VERSION=$(which /root/.nvm/versions/node/v24.20.0/bin/npm --version)
+echo "NPM version: $NPM_VERSION"
+
 
 # Run the global sura script and get the output
-npm run global-sura 3 10 >> $HOME/global-sura.log 2>&1
+/root/.nvm/versions/node/v24.20.0/bin/npm run global-sura 3 10 >> $HOME/global-sura.log 2>&1
 
 echo "Global sura script completed"
